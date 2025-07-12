79bbf2173bbb34ea2e4b53cb923a6bbe9ec42fd3
Prashant-koi <koiralaprashanta10@gmail.com>
2025-07-11 20:26:20 -0400

    typosfixed

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
       </h1>
       <div className="text-gray-300 leading-relaxed space-y-4">
         <p className="font-normal">
-          Hey there! I'm Prasant, a Computer Science student at who likes to build stuff that solves small but annoying issues. Currently working as a Web/App Intern At Optimal Answers, LLC.
+          Hey there! I'm Prasant, a Computer Science student who likes to build stuff that solves small but annoying issues. Currently working as a Web/App Intern At Optimal Answers, LLC.
         </p>
         <p className="font-normal">
           I've recently created a desktop widget that tracks what I'm up to in real-time (yeah, that's what powers the status monitor on this site!).
